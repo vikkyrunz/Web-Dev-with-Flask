4bf89ff6332aca378214f1ecf1f5f2786b995c49
Victor ukana <81874276+vikkyrunz@users.noreply.github.com>
2024-03-07 13:46:39 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template      
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello, Victor!"
+    return render_template("home.html")
+    
+@app.route("/salvador")
+def salvador():
+    return "Hello, Salvador"
     
 if __name__ == "__main__":
     app.run(debug=True)
